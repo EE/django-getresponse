8c0d46a09ad89da82d9a0f1a99160fdcfee9db7e
Krzysztof Jurewicz <krzysztof.jurewicz@laboratorium.ee>
2020-06-12 15:11:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+A Django email backend for GetResponse.
+
+## Usage
+
+Use it like a standard email backend with an additional feature that allows setting `.tag_id` attribute on an `EmailMessage` instance. The `.from_email` attribute is ignored (see settings instead).
+
+## Settings
+
+* `GETRESPONSE_API_TOKEN`
+* `GETRESPONSE_FROM_FIELD_ID`
+* `GETRESPONSE_ENDPOINT` — `https://api.getresponse.com/v3` by default.
---
+++ getresponse/mail.py
@@ -0,0 +1,77 @@
+import base64
+from email.mime.base import MIMEBase
+import threading
+from urllib.parse import urljoin
+
+from django.conf import settings
+from django.core.mail.backends.base import BaseEmailBackend
+import requests
+
+
+class GetResponseBackend(BaseEmailBackend):
+    def __init__(self):
+        self._session = None
+        self._endpoint = getattr(settings, 'GETRESPONSE_ENDPOINT', 'https://api.getresponse.com/v3')
+        self._lock = threading.RLock()
+
+    def send_messages(self, msgs):
+        count = 0
+        with self._lock, self:  # self is used to obtain connection
+            for msg in msgs:
+                if self._send_message(msg):
+                    count += 1
+        return count
+
+    def _send_message(self, msg):
+        payload = self.message_to_payload(msg)
+        url = urljoin(self._endpoint, 'transactional-emails')
+        response = self._session.post(url, payload)
+        return response.status_code == 201
+
+    def message_to_payload(self, msg):
+        payload = {
+            'fromField': {
+                # msg.from_email is ignored.
+                'fromFieldId': settings.GETRESPONSE_FROM_FIELD_ID,
+            },
+            'subject': msg.subject,
+            'content': {
+                'plain': msg.body,
+            },
+            'recipients': {
+                'to': msg.to,
+                'cc': msg.cc,
+                'bcc': msg.bcc,
+            },
+            'attachments': self.attachments_to_payload(msg.attachments),
+        }
+        if tag_id := getattr(msg, 'tag_id', None):
+            payload['tag'] = {
+                'tagId': tag_id,
+            }
+        return payload
+
+    def attachments_to_payload(self, attachments):
+        return [self.attachment_to_payload(attachment) for attachment in attachments]
+
+    def attachment_to_payload(self, attachment):
+        if isinstance(attachment, MIMEBase):
+            raise ValueError('MIMEBase attachments are currently not supported by GetResponse backend.')
+
+        filename, content, mimetype = attachment
+        return {
+            'fileName': filename,
+            'mimeType': mimetype,
+            'content': base64.b64encode(content if isinstance(content, bytes) else content.encode('utf-8')),
+        }
+
+    def open(self):
+        if self._session:
+            self.close()
+
+        self._session = requests.Session()
+        self._session.headers['X-Auth-Token'] = f'api-key {settings.GETRESPONSE_API_TOKEN}'
+
+    def close(self):
+        self._session.close()
+        self._session = None
